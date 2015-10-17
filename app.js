0299922631111a315b40cb536362843b80413bae
Chris Fuller <chris@cjwfuller.com>
2015-10-17 11:37:05 +0100

    connect to local DynamoDB and listTables

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var aws = require('aws-sdk');
 var app = express();
 
+dyn = new aws.DynamoDB({ endpoint: new aws.Endpoint('http://localhost:8000') });
+
+dyn.listTables(function (err, data) {
+	console.log('listTables', err, data);
 });
 
 app.listen(3000);
